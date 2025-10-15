adc613d95b8362f5f896deaac2118353faf7b782
Gaurav Sharma <gauravxv0410@gmail.com>
2025-10-15 21:52:56 +0530

    navbar

---
+++ portfolio/app/components/Home.js
@@ -0,0 +1,11 @@
+'use client'
+
+import Image from "next/image";
+import heroImg from "../../public/images/Home.png"; 
+
+export const HomeSection = () => {
+  return (
+  <>
+  </>
+  );
+};
---
+++ portfolio/app/page.js
@@ -0,0 +1,10 @@
+import { Navbar } from "./components/Navbar";
+import { HomeSection } from "./components/Home";
+export default function Home() {
+  return (
+   <>
+<Navbar/>
+<HomeSection/>
+   </>
+  );
+}
